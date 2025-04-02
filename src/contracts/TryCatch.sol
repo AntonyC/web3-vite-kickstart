7b773f07cc684355115e7438c3aa492053b2fc1d
Antony <antony.global@icloud.com>
2025-04-02 17:23:06 +0800

    Update TryCatch.sol

--- src/contracts/TryCatch.sol
+++ src/contracts/TryCatch.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.29;
 
 contract WillThrow {
     error ThisIsACustomError(string, string);
+
     function aFunction() public pure {
         // require(false, "Antony: error by require");
         // assert(false);
@@ -14,11 +15,12 @@ contract ErrorHandling {
     event ErrorLogging(string reason);
     event ErrorLogCode(uint reason);
     event ErrorLogBytes(bytes reason);
+
     function catchError() public {
         WillThrow will = new WillThrow();
         try will.aFunction() {
             //here we could do something if it works
-        } catch Error(string memory reason) { //get error by require
+        } catch Error(string memory reason) { // get error by require
             emit ErrorLogging(reason);
         } catch Panic(uint errorCode){ // get error by assert
             emit ErrorLogCode(errorCode);
